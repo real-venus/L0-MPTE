b12e94407c8b2321fa39d0ba66328bfa1cd72433
Gaussian120 <yamamotoyuki120@gmail.com>
2024-02-10 15:19:54 +0900

    fix IvestingWallet

--- test/mock/IVestingWallet.sol
+++ test/mock/IVestingWallet.sol
@@ -15,68 +15,60 @@
   /**
    * @dev Getter for the beneficiary address.
    */
-  function beneficiary() external view returns (address) {}
+  function beneficiary() external view returns (address);
 
   /**
    * @dev Getter for the start timestamp.
    */
-  function start() external view returns (uint256) {}
+  function start() external view returns (uint256);
 
   /**
    * @dev Getter for the vesting duration.
    */
-  function duration() external view returns (uint256) {}
+  function duration() external view returns (uint256);
 
   /**
    * @dev Amount of eth already released
    */
-  function released() external view returns (uint256) {
-      return _released;
-  }
+  function released() external view returns (uint256);
 
   /**
    * @dev Amount of token already released
    */
-  function released(address token) external view returns (uint256) {}
+  function released(address token) external view returns (uint256);
 
   /**
    * @dev Getter for the amount of releasable eth.
    */
-  function releasable() external view returns (uint256) {}
+  function releasable() external view returns (uint256);
 
   /**
    * @dev Getter for the amount of releasable `token` tokens. `token` should be the address of an
    * IERC20 contract.
    */
-  function releasable(address token) external view returns (uint256) {}
+  function releasable(address token) external view returns (uint256);
 
   /**
    * @dev Release the native token (ether) that have already vested.
    *
    * Emits a {EtherReleased} event.
    */
-  function release() external {}
+  function release() external;
 
   /**
    * @dev Release the tokens that have already vested.
    *
    * Emits a {ERC20Released} event.
    */
-  function release(address token) external {}
+  function release(address token) external;
 
   /**
    * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
    */
-  function vestedAmount(uint64 timestamp) external view returns (uint256) {}
+  function vestedAmount(uint64 timestamp) external view returns (uint256);
 
   /**
    * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
    */
-  function vestedAmount(address token, uint64 timestamp) external view returns (uint256) {}
-
-  /**
-   * @dev implementation of the vesting formula. This returns the amount vested, as a function of time, for
-   * an asset given its total historical allocation.
-   */
-  function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view returns (uint256) {}
+  function vestedAmount(address token, uint64 timestamp) external view returns (uint256);
 }
