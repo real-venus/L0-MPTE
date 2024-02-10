2f6e48fb45277aa4451cdc850e8ad80a3311570e
Gaussian120 <yamamotoyuki120@gmail.com>
2024-02-11 00:53:49 +0900

    complete mrpt token

--- src/MRPTToken.sol
+++ src/MRPTToken.sol
@@ -7,12 +7,21 @@ import "openzeppelin/finance/VestingWallet.sol";
 import "openzeppelin/token/ERC20/ERC20.sol";
 import "openzeppelin/token/ERC20/IERC20.sol";
 
+import {console} from "forge-std/console.sol";
+import {console2} from "forge-std/console2.sol";
+
 contract MRPTToken is OFT {
     uint public constant MAX_SUPPLY = 900e24;
     uint public mintable;
 
     address public FEE_RECEIVER = 0x085177Ca8B2b0947b80e31cA50CFfDfe32DBe5ED;
-    uint256 public TRANSFER_FEE = 50;
+    uint256 public TRANSFER_FEE = 5;
+
+    address public ecoSystemAddr;
+    address public marketingAddr;
+    address public stakingRewardsAddr;
+    address public teamAddr;
+    address public advisorsAddr;
 
     event VestingStarted(address beneficiaryAddress, address vestingWallet);
 
@@ -27,27 +36,28 @@ contract MRPTToken is OFT {
                 mintable = MAX_SUPPLY;
 
                 // Ecosystem - 20% 9 months cliff and 5% monthly for 20 months
-                _startVesting(ecoSystem, startVestingTimestamp, 9 * 30 days, 20 * 30 days, 2000);
+                ecoSystemAddr = _startVesting(ecoSystem, startVestingTimestamp, 9 * 30 days, 20 * 30 days, 2000);
 
                 // Marketing - 18% 5 months cliff and 5% monthly for 20 months
-                _startVesting(marketing, startVestingTimestamp, 5 * 30 days, 20 * 30 days, 1800);
+                marketingAddr = _startVesting(marketing, startVestingTimestamp, 5 * 30 days, 20 * 30 days, 1800);
 
                 // Staking Rewards - 21% Linear vesting for 60 Months
-                _startVesting(stakingRewards, startVestingTimestamp, 0, 60 * 30 days, 2100);
+                stakingRewardsAddr = _startVesting(stakingRewards, startVestingTimestamp, 0, 60 * 30 days, 2100);
 
                 // Team - 10% 12 months Cliff 5% monthly for 20 Months
-                _startVesting(team, startVestingTimestamp, 12 * 30 days, 20 * 30 days, 1000);
+                teamAddr = _startVesting(team, startVestingTimestamp, 12 * 30 days, 20 * 30 days, 1000);
 
                 // Advisors - 5% 10 Months cliff and 5% monthly for 20 months
-                _startVesting(advisors, startVestingTimestamp, 10 * 30 days, 20 * 30 days, 500);
+                advisorsAddr = _startVesting(advisors, startVestingTimestamp, 10 * 30 days, 20 * 30 days, 500);
             }
 
-    function _startVesting(address beneficiaryAddress, uint64 startVestingTimestamp, uint64 cliffSeconds, uint64 durationSeconds, uint64 proportion) internal {
+    function _startVesting(address beneficiaryAddress, uint64 startVestingTimestamp, uint64 cliffSeconds, uint64 durationSeconds, uint64 proportion) internal returns(address) {
         address vestingWallet = address(new VestingWallet(beneficiaryAddress, startVestingTimestamp + cliffSeconds, durationSeconds));
         uint amount = MAX_SUPPLY * proportion / 10000;
         mintable -= amount;
         _mint(vestingWallet, amount);
         emit VestingStarted(beneficiaryAddress, vestingWallet);
+        return vestingWallet;
     }
 
     function mint(address account, uint amount) external onlyOwner {
@@ -66,7 +76,7 @@ contract MRPTToken is OFT {
 
         // Get fee before transfer
         uint256 feeAmount = _amount * TRANSFER_FEE / 1000;
-        super.transferFrom(_msgSender(), FEE_RECEIVER, feeAmount);
+        super.transferFrom(_from, FEE_RECEIVER, feeAmount);
 
         _burn(_from, _amount);
         return _amount - feeAmount;
@@ -76,9 +86,17 @@ contract MRPTToken is OFT {
     function transferFrom(address from, address to, uint256 amount) public virtual override(ERC20, IERC20) returns (bool) {
         // Get fee before transfer
         uint256 feeAmount = amount * TRANSFER_FEE / 1000;
-        super.transferFrom(_msgSender(), FEE_RECEIVER, feeAmount);
-
+        super.transferFrom(from, FEE_RECEIVER, feeAmount);
         super.transferFrom(from, to, amount - feeAmount);
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public virtual override(ERC20, IERC20) returns (bool) {
+        // Get fee before transfer
+        uint256 feeAmount = amount * TRANSFER_FEE / 1000;
+        super.transfer(FEE_RECEIVER, feeAmount);
+
+        super.transfer(to, amount - feeAmount);
 
         return true;
     }
--- test/MRPTTokenTest.t.sol
+++ test/MRPTTokenTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import "test/mock/MockMRPTToken.sol";
+import "test/mock/IVestingWallet.sol";
 
 contract MRPTTokenTest is Test {
     MockMRPTToken public mrptToken;
@@ -16,21 +17,114 @@ contract MRPTTokenTest is Test {
     address stakingRewards = address(3);
     address team = address(4);
     address advisors = address(5);
-    address _lzEndpoint = address(6);
+    address alice = address(6);
+    address bob = address(7);
+    address _lzEndpoint = 0x66A71Dcef29A0fFBDBE3c6a460a3B5BC225Cd675;
 
     function setUp() public {
 
         mainnetFork = vm.createFork(vm.envString("MAINNET_RPC_URL"));
         optimismFork = vm.createFork(vm.envString("OPTIMISM_RPC_URL"));
-
-        mrptToken = new MockMRPTToken(startVestingTimestamp, ecoSystem, marketing, stakingRewards, team, advisors, _lzEndpoint);
     }
 
-    function test_transferFromFee() public {
+    // function test_transferFromFee() public {
+    //     vm.selectFork(mainnetFork);
+    //     assertEq(vm.activeFork(), mainnetFork);
+
+    //     vm.rollFork(19_196_173);
+    //     // mrptToken()
+    //     mrptToken.transfer()
+    // }
+
+    // function test_transferFromFee() public {
+    //     vm.selectFork(mainnetFork);
+    //     assertEq(vm.activeFork(), mainnetFork);
+
+    //     vm.rollFork(19_196_173);
+    //     // mrptToken()
+    //     // mrptToken.transfer()
+    // }
+
+    function test_vest_peoriod() public {
         vm.selectFork(mainnetFork);
         assertEq(vm.activeFork(), mainnetFork);
 
-        // mrptToken()
+        vm.rollFork(19_196_173);
+        
+        // vm.startPrank(ecoSystem);
+        // console.log("----start----");
+        // console.log(IVestingWallet(mrptToken.marketingAddr()).start());
+        // console.log("----released----");
+        // console.log(IVestingWallet(mrptToken.marketingAddr()).released(address(mrptToken)));
+        // console.log("----releasable----");
+        // console.log(IVestingWallet(mrptToken.marketingAddr()).releasable(address(mrptToken)));
+        // console.log("----beneficiary----");
+        // console.log(IVestingWallet(mrptToken.advisorsAddr()).beneficiary());
+        // console.log("----duration----");
+        // console.log(IVestingWallet(mrptToken.advisorsAddr()).duration());
+        // console.log("----vestedAmount----");
+        // console.log(IVestingWallet(mrptToken.advisorsAddr()).vestedAmount(address(mrptToken), uint64(block.timestamp)));
+
+        mrptToken = new MockMRPTToken(uint64(block.timestamp), ecoSystem, marketing, stakingRewards, team, advisors, _lzEndpoint);
+
+        assertEq(IVestingWallet(mrptToken.ecoSystemAddr()).beneficiary(), ecoSystem);
+        assertEq(IVestingWallet(mrptToken.ecoSystemAddr()).duration(), 20 * 30 days);
+        assertEq(mrptToken.balanceOf(mrptToken.ecoSystemAddr()), 900e24 * 0.2);
+
+        assertEq(IVestingWallet(mrptToken.marketingAddr()).beneficiary(), marketing);
+        assertEq(IVestingWallet(mrptToken.marketingAddr()).duration(), 20 * 30 days);
+        assertEq(mrptToken.balanceOf(mrptToken.marketingAddr()), 900e24 * 0.18);
+
+        assertEq(IVestingWallet(mrptToken.stakingRewardsAddr()).beneficiary(), stakingRewards);
+        assertEq(IVestingWallet(mrptToken.stakingRewardsAddr()).duration(), 60 * 30 days);
+        assertEq(mrptToken.balanceOf(mrptToken.stakingRewardsAddr()), 900e24 * 0.21);
+
+        assertEq(IVestingWallet(mrptToken.teamAddr()).beneficiary(), team);
+        assertEq(IVestingWallet(mrptToken.teamAddr()).duration(), 20 * 30 days);
+        assertEq(mrptToken.balanceOf(mrptToken.teamAddr()), 900e24 * 0.1);
+
+        assertEq(IVestingWallet(mrptToken.advisorsAddr()).beneficiary(), advisors);
+        assertEq(IVestingWallet(mrptToken.advisorsAddr()).duration(), 20 * 30 days);
+        assertEq(mrptToken.balanceOf(mrptToken.advisorsAddr()), 900e24 * 0.05);
+
+        IVestingWallet(mrptToken.ecoSystemAddr()).release(address(mrptToken));
+
+        assertEq(mrptToken.balanceOf(ecoSystem), 0);
+
+        vm.warp(block.timestamp + 9 * 30 days + 1 * 30 days);
+        console.log(IVestingWallet(mrptToken.ecoSystemAddr()).vestedAmount(address(mrptToken), uint64(block.timestamp)));
+        // after vesting period, 20% can be released
+        IVestingWallet(mrptToken.ecoSystemAddr()).release(address(mrptToken));
+
+        assertEq(mrptToken.balanceOf(ecoSystem), 900e24 * 0.2 / 20 - 900e24 * 0.2 / 20 * 0.005);
+
+    }
+
+    function test_transferFee() public {
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+
+        vm.rollFork(19_196_173);
+
+        mrptToken = new MockMRPTToken(uint64(block.timestamp), ecoSystem, marketing, stakingRewards, team, advisors, _lzEndpoint);
+
+        mrptToken.mint(address(alice), 10 ether);
+        
+        // transferFrom test
+        vm.prank(alice);
+        mrptToken.approve(address(this), 1 ether);
+        mrptToken.transferFrom(alice, bob, 1 ether);
+
+        // 0.5% fee
+        assertEq(mrptToken.balanceOf(mrptToken.FEE_RECEIVER()), 5e15);
+        assertEq(mrptToken.balanceOf(bob), 995e15);
+        
+        vm.prank(alice);
+        mrptToken.transfer(bob, 1 ether);
+
+        // 0.5% fee
+        assertEq(mrptToken.balanceOf(mrptToken.FEE_RECEIVER()), 1e16);
+        assertEq(mrptToken.balanceOf(bob), 199e16);
     }
 
     // function testFuzz_SetNumber(uint256 x) public {
